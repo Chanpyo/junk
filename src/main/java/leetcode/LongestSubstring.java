e04898c1fadcdef32055bc78e87a8022c1dbffed
hong <gzalzloqh@gmail.com>
2020-12-07 19:35:22 +0900

    [leetcode] substring - add remaining

--- src/main/java/leetcode/LongestSubstring.java
+++ src/main/java/leetcode/LongestSubstring.java
@@ -12,6 +12,7 @@ public class LongestSubstring {
         subStringWoRepeat("abcabcbb");
         subStringWoRepeat("bbbbb");
         subStringWoRepeat("pwwkew");
+        subStringWoRepeat("abcadfe");
     }
 
     private static void subStringWoRepeat(String original) {
@@ -29,17 +30,26 @@ public class LongestSubstring {
         char[] candidate2 = new char[0];
 
         for (int i=0; i<separated.length; i++) {
-            if (candidate2.length == 0 || !contains(candidate2, separated[i])) {
+            int repeatIndex = contains(candidate2, separated[i]);
+            if (candidate2.length == 0 || repeatIndex < 0) {
                 char[] newCandi2 = new char[candidate2.length + 1];
                 for (int j = 0; j < candidate2.length; j++) {
                     newCandi2[j] = candidate2[j];
                 }
                 newCandi2[candidate2.length] = separated[i];
                 candidate2 = newCandi2;
-            } else if (candidate1.length < candidate2.length) {
-                candidate1 = candidate2;
-                candidate2 = new char[1];
-                candidate2[0] = separated[i];
+            } else {
+                if (candidate1.length < candidate2.length) {
+                    candidate1 = candidate2;
+                }
+
+                char[] newCandi2 = new char[candidate2.length - repeatIndex];
+                for (int j=0; j<newCandi2.length - 1; j++) {
+                    newCandi2[j] = candidate2[j + repeatIndex + 1];
+                }
+                newCandi2[newCandi2.length - 1] = separated[i];
+                candidate2 = newCandi2;
+
             }
         }
         if (candidate1.length < candidate2.length) {
@@ -55,15 +65,15 @@ public class LongestSubstring {
         System.out.println("\n===========================");
     }
 
-    private static boolean contains(char[] chars, char target) {
+    private static int contains(char[] chars, char target) {
         if (chars == null || chars.length < 1) {
-            return false;
+            return -1;
         }
 
         for (int i=0; i<chars.length; i++) {
-            if (chars[i] == target) return true;
+            if (chars[i] == target) return i;
         }
 
-        return false;
+        return -1;
     }
 }
